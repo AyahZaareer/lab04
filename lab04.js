9d8ce67daf01442685499e57ec732955b3fc05aa
AyahZaareer <ayah@LTUCSTUDENT-L075.localdomain>
2021-03-04 14:13:36 +0200

    chik informatin

---
+++ lab04.js
@@ -0,0 +1,25 @@
+var user_name = prompt("please enter your name");
+
+if(user_name == "Ayah")
+{ 
+    alert("Hi Ayah");
+    var password = prompt("please enter your password");
+    if(password =="1994")
+    {
+        console.log("Welcome Ayah");
+        document.write('Hi Ayah');
+        var inf = user_name + password ;
+     console.log("Ayah 1994");
+    }
+
+}
+else
+{
+    alert("No Match");
+}
+
+   
+  
+
+
+
